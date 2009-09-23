9725c4576ecb2b10001bbb7337f4511b4a092387
pH <pedrohfranceschi@gmail.com>
2009-09-22 21:28:39 -0300

    Added a RubyCocoa example.

---
+++ example/rubycocoa/MyController.rb
@@ -0,0 +1,35 @@
+# RubyController
+
+require 'osx/cocoa'
+require 'phtwitvid'
+
+class RubyController < OSX::NSObject
+
+  ib_outlets :tf_username, :tf_password, :tf_path, :tf_description, :radio, :tf_source, :spinner;
+  
+  ib_action :btnClicked do |sender|
+    @spinner.startAnimation 0
+    uploadVideo(@tf_username.stringValue, @tf_password.stringValue, @tf_path.stringValue, @tf_description.stringValue, @tf_source.stringValue)
+	  @spinner.stopAnimation 0
+  end
+  
+  def uploadVideo(username, password, path, description, source)  
+	twitvid = PHTwitVid.new();
+	token = twitvid.get_token(username, password)
+	if token == 'Invalid username/password'
+		show_alert("Error", "Invalid username or password");
+	else
+	video = twitvid.upload(token, description, path, 'upload', source)	
+	show_alert("Success", "Your video was successfully uploaded!\nVideo URL: #{video['media_url']}")
+	end
+	end
+	
+  def show_alert(title, message)
+	alert = OSX::NSAlert.alloc.init;
+	alert.setMessageText(title)
+	alert.setInformativeText(message);
+	alert.setAlertStyle(OSX::NSInformationalAlertStyle)
+	alert.beginSheetModalForWindow(@mainWindow,:modalDelegate, self,:didEndSelector, nil,:contextInfo, nil)
+	end
+
+end
